41bc901af4d9ae3d14a2e4bac7e37377a726abce
Andersonmia2222 <mia.anderson2026@gbwl.org>
2025-03-03 17:11:39 +0000

    updates

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+/*This is a MULTI-LINE comment in JS 
+Below is an example of a JS STATMENT
+(instruction/command)
+Statments end with a SEMICOLON (;)
+*/
+console.log("Welcome to JavaScript!!!");
+
+//This is a single line comment
+// console.log is a FUNCTION to display information
+console.log("this is the console/output");
+
+// DECLARE a VARIABLE to hold data 
+//use the LET keyword to create the variable
+let pokemon;
+
+// ASSIGN a VALUE to that variable
+//like packing an item into a box
+pokemon= "Pikachu" ;
+
+// Can DECLEAR + ASSIGN in one statement 
+let name = "Mia" ;
